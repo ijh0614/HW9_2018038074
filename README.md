76c414345c753714085ee767e340a0e173722028
ijh0614 <63185555+ijh0614@users.noreply.github.com>
2021-05-02 13:45:21 +0900

    free_and_searchIterative_create
    two function complete

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## To build
 
-- [] void inorderTraversal(Node* ptr); /* recursive inorder traversal \*/
-- [] void preorderTraversal(Node* ptr); /* recursive preorder traversal \*/
-- [] void postorderTraversal(Node* ptr); /* recursive postorder traversal \*/
+- [O] int freeBST(Node* head); /* free all memories allocated to the tree \*/
 - [] int insert(Node* head, int key); /* insert a node to the tree \*/
+- [] void preorderTraversal(Node* ptr); /* recursive preorder traversal \*/
+- [] void inorderTraversal(Node* ptr); /* recursive inorder traversal \*/
+- [] void postorderTraversal(Node* ptr); /* recursive postorder traversal \*/
 - [] int deleteLeafNode(Node* head, int key); /* delete the leaf node for the key \*/
 - [] Node* searchRecursive(Node* ptr, int key); /_ search the node for the key _/
-- [] Node* searchIterative(Node* head, int key); /_ search the node for the key _/
-- [] int freeBST(Node* head); /* free all memories allocated to the tree \*/
+- [O] Node* searchIterative(Node* head, int key); /_ search the node for the key _/
--- binary-search-tree-1.c
+++ binary-search-tree-1.c
@@ -11,33 +11,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node
+typedef struct node //노드 구조체
 {
     int key;
-    struct node *left;
-    struct node *right;
+    struct node *left;  //왼쪽 자식노드 주소 저장
+    struct node *right; //오른쪽 자식노드 주소 저장
 } Node;
 
-int initializeBST(Node **h);
+int initializeBST(Node **h); //이중포인터를 매개변수로 받는다.
 
 /* functions that you have to implement */
-void inorderTraversal(Node *ptr);           /* recursive inorder traversal */
-void preorderTraversal(Node *ptr);          /* recursive preorder traversal */
-void postorderTraversal(Node *ptr);         /* recursive postorder traversal */
+//노드 삽입. 기존에 있는 노드와 크기비교도 필요함
 int insert(Node *head, int key);            /* insert a node to the tree */
+
+
+void inorderTraversal(Node *ptr);           /* recursive inorder traversal */
+
+
+void postorderTraversal(Node *ptr);         /* recursive postorder traversal */
+
+
+void preorderTraversal(Node *ptr);          /* recursive preorder traversal */
+
+
 int deleteLeafNode(Node *head, int key);    /* delete the leaf node for the key */
+
+/*입력한 key 값이 tree에 저장되어있는지 확인
+재귀를 사용한다.
+반환값은 찾았을 경우 주소를, 못 찾은 경우 NULL을 반환한다.*/
 Node *searchRecursive(Node *ptr, int key);  /* search the node for the key */
+
+/*입력한 key 값이 tree에 저장되어있는지 확인
+non-recursive 방식으로 반복문을 사용한다.
+반환값은 찾았을 경우 주소를, 못 찾은 경우 NULL을 반환한다.*/
 Node *searchIterative(Node *head, int key); /* search the node for the key */
+
+//모든 노드 동적할당 해제
 int freeBST(Node *head);                    /* free all memories allocated to the tree */
 
 /* you may add your own defined functions if necessary */
 
 int main()
 {
-    char command;
-    int key;
-    Node *head = NULL;
-    Node *ptr = NULL; /* temp */
+    char command;      //사용자 메뉴 입력
+    int key;           //노드의 값 저장
+    Node *head = NULL; //헤드 노드의 주소 저장
+    Node *ptr = NULL;  /* temp */
 
     do
     {
@@ -59,7 +78,7 @@ int main()
         {
         case 'z':
         case 'Z':
-            initializeBST(&head);
+            initializeBST(&head); //노드형의 주소를 저장하는 포인터 변수의 주소를 넣어줌.
             break;
         case 'q':
         case 'Q':
@@ -120,7 +139,7 @@ int main()
     return 1;
 }
 
-int initializeBST(Node **h)
+int initializeBST(Node **h) //main에 있는 head에 직접 값을 보내준다.
 {
 
     /* if the tree is not empty, then remove all allocated nodes from the tree*/
@@ -147,8 +166,20 @@ void postorderTraversal(Node *ptr)
 {
 }
 
+/*1. 입력받은 key가 기존에 있는 노드인가 -> serch의 반환값이 null이 아닐경우 : 이미 있는 key입니다. [주소:]로 전처리
+2. 노드를 삽입할 주소 찾기
+3. 노드 생성 및 값 대입*/
 int insert(Node *head, int key)
 {
+    Node* ptr = NULL;
+    ptr = searchIterative(head, key);//전처리 - 이미 key값이 저장되어 있는가?
+    if(ptr != NULL){
+        printf("트리에 이미 존재하는 key값입니다. [주소 : %p]\n\n", ptr);
+        return 0;
+    }
+
+
+    return 0;
 }
 
 int deleteLeafNode(Node *head, int key)
@@ -161,8 +192,31 @@ Node *searchRecursive(Node *ptr, int key)
 
 Node *searchIterative(Node *head, int key)
 {
+    Node* node = head->left;//node에 루트노드 주소 저장
+    while(node){
+        if(node->key == key){//노드의 key값이 입력한 key값과 같다면
+            return node;//노드의 주소 반환
+        }
+        if(node->key > key){//현재 읽고있는 노드의 key값이 찾는 key값보다 크면
+            node = node->left;//왼쪽 자식노드 탐색
+        }
+        else{//그렇지 않으면
+            node = node->right;//오른쪽 자식노드 탑색
+        }
+    }
+
+    return NULL;//끝까지 못 찾은 경우
 }
 
-int freeBST(Node *head)
+int freeBST(Node *head)//모든 노드를 해제해주어야 함. 후위순회 방식에서 방문시 동적할당을 해제하는 방식을 사용한다.
 {
+    if(head == head->right){//헤드노드일 때
+        free(head);
+        return 0;//바로 해제해주고 탈출
+    }
+    if(head){//맨 처음에는 헤드노드에서 루트노드로 내려감.
+        freeBST(head->left);//재귀적
+        freeBST(head->right);//헤드노드에서 무한루프에 빠지나?
+        free(head);
+    }
 }
